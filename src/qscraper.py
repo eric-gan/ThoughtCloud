43e35b66592b0d639e1dac469904b600c148415e
eric-gan <32603876+eric-gan@users.noreply.github.com>
2018-08-05 16:16:45 -0400

    added scraper

---
+++ src/qscraper.py
@@ -0,0 +1,13 @@
+import urllib.request
+from bs4 import BeautifulSoup
+url = 'https://conversationstartersworld.com/philosophical-questions/'
+html = urllib.request.urlopen(url)
+
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.prettify())
+data = []
+for line in soup.find_all('p'):
+    new_entry = line.text
+    if len(new_entry) != 0 and new_entry[len(new_entry) - 1] == '?':
+        data.append(new_entry)
+print(data)
