ea5870e408f98179ea805d824d9cc2e3da74ecd6
eric-gan <32603876+eric-gan@users.noreply.github.com>
2018-08-06 02:49:08 -0400

    reworked to implemnt flask

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello, World!"
-  
+
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
+
+# @app.route("/")
+# def index():
+#     #return app.send_static_file('index.html')
+#     return render_template('index.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
-  app.run()
+    app.run()
