c7d696e9cba635f9f18df2e47404376d2a29b73c
eric-gan <32603876+eric-gan@users.noreply.github.com>
2018-08-06 02:50:01 -0400

    Minor fixes and refactoring

--- src/main.py
+++ src/main.py
@@ -3,15 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# @app.route("/")
-# def hello():
-#     return "Hello, World!"
-
-# @app.route("/")
-# def index():
-#     #return app.send_static_file('index.html')
-#     return render_template('index.html')
-
 @app.route('/')
 def index():
     return render_template('index.html')
