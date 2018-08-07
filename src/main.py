7e6f8d3ce54878b88373d1c298f1573ab3b90b57
eric-gan <32603876+eric-gan@users.noreply.github.com>
2018-08-07 02:22:59 -0400

    Minor fixes and refactoring

--- src/main.py
+++ src/main.py
@@ -4,14 +4,17 @@ from qscraper import scrape_questions, scrape_quora
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/dashboard/')
 def dashboard():
-	queries = scrape_quora()
-	return render_template('dashboard.html', queries=queries)
+    queries = scrape_quora()
+    return render_template('dashboard.html', queries=queries)
+
 
 if __name__ == "__main__":
     app.run()
