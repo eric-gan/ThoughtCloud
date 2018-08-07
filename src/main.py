e56c6a6af77fb031f036ff25cb8598dc80c386d7
vliu15 <vliu15@Home-Dell-13.localdomain>
2018-08-06 22:00:18 -0700

    scrape in main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from qscraper import scrape
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/')
+def dashboard():
+	queries = scrape()
+	return render_template('dashboard.html', queries=queries)
 
 if __name__ == "__main__":
     app.run()
