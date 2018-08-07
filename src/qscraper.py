419b8dbccad43ce8ad5db1b71122eab549bbce92
eric-gan <32603876+eric-gan@users.noreply.github.com>
2018-08-07 00:56:54 -0400

    questions

--- src/qscraper.py
+++ src/qscraper.py
@@ -4,15 +4,18 @@ url = 'https://conversationstartersworld.com/philosophical-questions/'
 html = urllib.request.urlopen(url)
 
 soup = BeautifulSoup(html, 'html.parser')
-data = []
 topics = []
-for line in soup.find_all('p'):
-    new_entry = line.text
-    if len(new_entry) != 0 and new_entry[len(new_entry) - 1] == '?':
-        data.append(new_entry)
 
-for line in soup.find_all('h2'):
-    new_entry = line.text
-    print(new_entry)
-    if 'questions' in new_entry:
-        topics.append(new_entry)
+
+def scrape_questions():
+    data = []
+    for line in soup.find_all('p'):
+        new_entry = line.text
+        if len(new_entry) != 0 and new_entry[len(new_entry) - 1] == '?':
+            data.append(new_entry)
+    return data
+# for line in soup.find_all('h2'):
+#     new_entry = line.text
+#     print(new_entry)
+#     if 'questions' in new_entry:
+#         topics.append(new_entry)
