6f45f10b04d36d7513d0200e8012c465ecb1a174
eric-gan <32603876+eric-gan@users.noreply.github.com>
2018-08-07 01:46:01 -0400

    Minor fixes and refactoring

--- src/qscraper.py
+++ src/qscraper.py
@@ -1,21 +1,24 @@
 import urllib.request
 from bs4 import BeautifulSoup
-url = 'https://conversationstartersworld.com/philosophical-questions/'
-html = urllib.request.urlopen(url)
-
-soup = BeautifulSoup(html, 'html.parser')
-topics = []
-
+from selenium import webdriver
+import time
 
 def scrape_questions():
+    url = 'https://conversationstartersworld.com/philosophical-questions/'
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
     data = []
     for line in soup.find_all('p'):
         new_entry = line.text
         if len(new_entry) != 0 and new_entry[len(new_entry) - 1] == '?':
             data.append(new_entry)
     return data
-# for line in soup.find_all('h2'):
-#     new_entry = line.text
-#     print(new_entry)
-#     if 'questions' in new_entry:
-#         topics.append(new_entry)
+
+def scrape_livesite():
+    driver = webdriver.Chrome()
+    url = 'https://www.quora.com/topic/Astrophysics'
+    driver.geturl(url)
+    html = driver.page_source
+    print(html)
+
+scrape_livesite()
