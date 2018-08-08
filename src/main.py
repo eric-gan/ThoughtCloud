01f5dce6df84915fc3350ec94dae2763043fcb6c
eric-gan <32603876+eric-gan@users.noreply.github.com>
2018-08-08 01:01:47 -0400

    finished backend for search

--- src/main.py
+++ src/main.py
@@ -1,31 +1,35 @@
 from flask import Flask, render_template, url_for, request
-from wtforms import Form, TextAreaField, validators
 from qscraper import scrape_questions, scrape_quora
 
 
 app = Flask(__name__)
 
-class SearchForm(Form):
-	inputString = TextAreaField('Query', [validators.DataRequired()])
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/dashboard/')
-def dashboard():
-	return render_template('dashboard.html', queries=[])
+@app.route('/results/')
+def results(queries=None):
+    print(queries)
+    return render_template('results.html', queries=queries)
 
-@app.route('/results/', methods=['POST'])
-def results():
-	form = SearchForm(request.form)
-	queries = []
-	if request.method == 'POST' and form.validate():
-		search = request.form['search']
-		queries = scrape_quora(search)
-		return redirect(url_for('dashboard'))
-	return render_template('dashboard.html', queries=queries)
+
+@app.route('/search/')
+def search():
+    print('started results')
+    return render_template('search.html')
+
+
+@app.route('/search/', methods=['POST'])
+def search_requested():
+    print('got here')
+    search_results = []
+    search_input = request.form['search']
+    search_results = scrape_quora(search_input)
+    return results(queries=search_results)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
